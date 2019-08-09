4c7213e0abf0300f092d0d7167943f919e326bcf
rsookram <rashadsookram@gmail.com>
2019-08-09 11:14:22 -0400

    Only allow collapsing by swiping down from the top

--- app/src/main/java/io/github/rsookram/notepad/view/CollapseInterceptor.kt
+++ app/src/main/java/io/github/rsookram/notepad/view/CollapseInterceptor.kt
@@ -7,8 +7,12 @@ import me.saket.inboxrecyclerview.page.OnPullToCollapseInterceptor
 class CollapseInterceptor(private val scrollableChild: View) : OnPullToCollapseInterceptor {
 
     override fun invoke(downX: Float, downY: Float, upwardPull: Boolean): InterceptResult {
-        val directionInt = if (upwardPull) 1 else -1
-        val canScrollFurther = scrollableChild.canScrollVertically(directionInt)
+        // Only allow collapsing by swiping down by from the top
+        if (upwardPull) {
+            return InterceptResult.INTERCEPTED
+        }
+
+        val canScrollFurther = scrollableChild.canScrollVertically(-1)
         return if (canScrollFurther) InterceptResult.INTERCEPTED else InterceptResult.IGNORED
     }
 }
